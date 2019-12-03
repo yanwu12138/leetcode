e62b617d5852ce5062777fbf7dd05612312fbf9b
许保锋 <yanwu0527@163.com>
2019-12-03 17:42:04 +0800

    提交：0001_两数之和

---
+++ com.yanwu.demo.leetcode.t0001to0100/t0001.go
@@ -0,0 +1,37 @@
+/**
+ * @author <a herf="mailto:yanwu0527@163.com">XuBaofeng</a>
+ * @date 2019-12-03 9:18.
+ * <p>
+ * description: 两数之和
+ * * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * * 示例:
+ * *    给定 nums = [2, 7, 11, 15], target = 9
+ * *    因为 nums[0] + nums[1] = 2 + 7 = 9
+ * *    所以返回 [0, 1]
+ * *    <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println("twoSum result: ", twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			// ----- 找到两个数相加等于target，将角标返回
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
